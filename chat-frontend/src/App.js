42998c7c0f842e484f4b98698d6297884e477684
‘Juliano <julianoperins@gmail.com>
2021-05-25 14:40:47 -0400

    Created routes for all pagesTwo

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -3,13 +3,20 @@ import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import Chat from "./components/Chat/Chat";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Chat />
-      <Login />
-      <Register />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Chat} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="*" render={() => <h1>404 page not found</h1>} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
