66726f0f09b7bfd855ba9c9df48ee60a21d61a9e
‘Juliano <julianoperins@gmail.com>
2021-05-25 14:30:44 -0400

    Created the folder structure

---
+++ chat-frontend/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import Chat from "./components/Chat/Chat";
+
+function App() {
+  return (
+    <div className="App">
+      <Chat />
+      <Login />
+      <Register />
+    </div>
+  );
+}
+
+export default App;
---
+++ chat-frontend/src/components/Chat/Chat.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Chat = () => {
+  return (
+    <div>
+      <h1>chaat</h1>
+    </div>
+  );
+};
+
+export default Chat;
