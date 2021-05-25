0222dfb0e977f3533961803aa91008a994a37439
‘Juliano <julianoperins@gmail.com>
2021-05-24 20:38:01 -0400

    Created validators using express validator

--- chat-backend/router/auth.js
+++ chat-backend/router/auth.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
+const { login, register } = require("../controllers/authController");
+const { validate } = require("../validators");
+const { rules: registrationsRules } = require("../validators/auth/register");
+const { rules: loginRules } = require("../validators/auth/login");
 
-router.post("/login", (req, res) => {
-  res.send(["Login Screen", req.body]);
-});
+router.post("/login", [loginRules, validate], login);
 
-router.post("/register", (req, res) => {
-  res.send("Register screen");
-});
+router.post("/register", [registrationsRules, validate], register);
 
 module.exports = router;
